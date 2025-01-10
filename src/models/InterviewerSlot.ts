b2b242624febe8b9d767168f7341a8e42a9844ad
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-10 20:46:15 +0530

    interviewer slot schema

---
+++ src/models/InterviewerSlot.ts
@@ -0,0 +1,44 @@
+import mongoose,{Schema,Document} from "mongoose";
+
+
+export interface InterviewerSlot extends Document{
+    userId:mongoose.Schema.Types.ObjectId,
+    date:string,
+    time:string,
+    meetLink:string,
+    feedback:string,
+    isVerified?:boolean
+}
+
+const InterviewerSlotSchema=new Schema<InterviewerSlot>({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    meetLink:{
+        type:String,
+        default:""
+    },
+    feedback:{
+        type:String,
+        default:""
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+        required:false
+    }
+})
+
+const InterviewerSet=mongoose.model<InterviewerSlot>("InterrviewerSet",InterviewerSlotSchema);
+
+export default InterviewerSet;

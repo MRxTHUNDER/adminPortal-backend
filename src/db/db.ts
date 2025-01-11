ee4ad066ab6cdae4942001edb399bc8f69f4d535
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-11 14:22:09 +0530

    adding project architecture and updated folders

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import { MONGO_URI } from "../config/config";
 
 
 dotenv.config();
 
-const mongoURI = process.env.MONGO_URI
-console.log(mongoURI)
+console.log(MONGO_URI)
 
 
-if(!mongoURI){
+if(!MONGO_URI){
     console.error("MONGO URI is not defined");
     process.exit(1);
 }
 
 
 const mongoConnect = ():void=>{
-    mongoose.connect(mongoURI).then(()=>{
+    mongoose.connect(MONGO_URI).then(()=>{
         console.log("Database connected");
     }).catch((error)=>{
         console.error('Error connecting to Database: ',error)

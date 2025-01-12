b3ad0dee5bb5af159cae9ad745bca647fd850d1c
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-12 23:17:40 +0530

    configs updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,3 +3,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const MONGO_URI:string = process.env.MONGO_URI ||'';
+export const JWT_SECRET = process.env.JWT_SECRET
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD

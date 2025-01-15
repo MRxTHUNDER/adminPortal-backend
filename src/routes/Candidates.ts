ab7bb684ca35a6b54b7ccdae2b0f4dce3c19da31
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-15 15:27:21 +0530

    candidate details put api

--- src/routes/Candidates.ts
+++ src/routes/Candidates.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getCandidateInterviews, getCandidates } from '../controllers/candidates';
+import { editCandidateInterview, getCandidateInterviews, getCandidates } from '../controllers/candidates';
 import { authenticate } from '../auth/auth';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get("/getAllCandidates",authenticate,getCandidates);
 router.get("/getCandidateInfo/:id",authenticate,getCandidateInterviews);
+router.put("/editCandidateInterview/:id", authenticate, editCandidateInterview);
 
 export default router;

3f13ab52ed5bc6d4087863de374cf53b6738cd50
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-16 13:10:24 +0530

    interview slots fetch for each interviewers api

--- src/routes/Interviewers.ts
+++ src/routes/Interviewers.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { authenticate } from '../auth/auth';
-import { getInterviewers } from '../controllers/interviewers';
+import { getInterviewers, getInterviewerSlots } from '../controllers/interviewers';
 
 const router = express.Router();
 
 
 router.get("/getAllInterviewers",authenticate,getInterviewers);
+router.get("/getInterviewSlots/:userId",authenticate,getInterviewerSlots);
 
 export default router;
